868fe73f5e0bf6d0363c1657731fe3cf17b6618f
Michael James <micrjames921@gmail.com>
2023-12-31 21:40:28 -0700

    added a few extra test cases for 'bubble sort'

--- sorting/bubble-sort.ts
+++ sorting/bubble-sort.ts
@@ -1,7 +1,9 @@
 exports.bubS = (arr: number[]): number[] => {
    const n = arr.length;
    // let temp: number;
+   let swapped: boolean;
    for(let i = 0; i < n - 1; i++) { 
+	  swapped = false;
 	  for(let j = 0; j < n - i - 1; j++) {
 		 if(arr[j] > arr[j + 1]) {
 			/*
@@ -10,9 +12,12 @@ exports.bubS = (arr: number[]): number[] => {
 			arr[j + 1] = temp;
 		   */
 		   [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+
+		   swapped = true;
 		 }
 	  }
+
+	  if(swapped == false) break;
    }
-   console.log(arr);
    return arr;
 };
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,14 @@
 const { bubS } = require("../sorting/bubble-sort");
 
+const cases: number[][] = [
+   [1, 2, 3, 4, 5],
+   [1, 3, 2, 4, 5],
+   [1, 2, 4, 3, 5],
+   [5, 4, 3, 2, 1]
+];
 describe("An array sorted by 'bubble sort'.", () => {
-   test("Should be in ascending order.", () => {
+   test.each(cases)
+   	("Should be in ascending order.", () => {
 	  const expected = [1, 2, 3, 4, 5];
 	  expect(bubS([5, 4, 3, 2, 1])).toEqual(
 		 expect.arrayContaining(expected)
