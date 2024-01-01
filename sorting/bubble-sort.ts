969e2164c80d1b87010e54173bae9c9fee88ff77
Michael James <micrjames921@gmail.com>
2023-12-31 20:18:40 -0700

    implemented 'bubble-sort' using swap with and without es6 syntax

--- sorting/bubble-sort.ts
+++ sorting/bubble-sort.ts
@@ -1,3 +1,18 @@
 exports.bubS = (arr: number[]): number[] => {
-   return arr.sort((a, b) => a - b);
+   const n = arr.length;
+   // let temp: number;
+   for(let i = 0; i < n - 1; i++) { 
+	  for(let j = 0; j < n - i - 1; j++) {
+		 if(arr[j] > arr[j + 1]) {
+			/*
+			temp = arr[j];
+			arr[j] = arr[j + 1];
+			arr[j + 1] = temp;
+		   */
+		   [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+		 }
+	  }
+   }
+   console.log(arr);
+   return arr;
 };
