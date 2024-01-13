f139acb14f6a6aa0dd062dce52839c5e95b62ac6
Michael James <micrjames921@gmail.com>
2024-01-13 13:39:06 -0700

    completed testing on 'bubble-sort method'

---
+++ tests/bubS.test.ts
@@ -0,0 +1,27 @@
+const { bubS } = require("../sorting/bubble-sort");
+const { Range } = require("../../Range/Range");
+const { shuffle } = require("../../shuffle/shuffle");
+const { Random } = require("../../Random/Random");
+
+const numCases = new Random(4, 10).integer;
+const seqSize = 5;
+let cases: number[][] = [];
+cases[0] = [...new Range(seqSize)];
+
+for(let i = 1; i < numCases; i++) {
+   cases[i] = shuffle([...cases[0]]);
+}
+
+describe("An array sorted by 'bubble sort'.", () => {
+   test("Should be an array.", () => {
+	  for(let i = 0; i < cases.length; i++) {
+	  	 expect(bubS(cases[i])).toEqual(expect.arrayContaining([expect.anything()]));
+	  }
+   });
+   test("Should be in ascending order.", () => {
+	  const expected = [0, 1, 2, 3, 4];
+	  for(let i = 0; i < cases.length; i++) {
+	  	 expect(bubS(cases[i])).toEqual(expected);
+	  }
+   });
+});
---
+++ tests/selS.test.ts
@@ -0,0 +1,6 @@
+const { selS } = require("../sorting/selection-sort");
+
+describe("An array sorted by 'selection sort'", () => {
+   test("Should be in ascending order.", () => {
+   });
+});
